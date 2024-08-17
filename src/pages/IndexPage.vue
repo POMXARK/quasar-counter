7a480e5249638ded2b5592904156084d620e7cca
Роман Бушуев <bushuev@benefitty.ru>
2024-08-17 11:26:42 +0400

    feat: Name Input & Reset Button Design
    Добавил кнопку сброса, текстовое поле.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page class="flex flex-center text-white">
+    <div class="row">
+      <q-input
+        input-class="text-center text-h5 text-white"
+        color="teal"
+        placeholder="Counter"
+        filled
+      />
+    </div>
     <div class="row full-width items-center">
       <div class="col text-center">
         <q-btn
@@ -19,8 +27,22 @@
         />
       </div>
     </div>
+    <div class="row">
+      <q-btn
+        icon="restart_alt"
+        size="xl"
+        round
+      />
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
 
 <script setup>
 
