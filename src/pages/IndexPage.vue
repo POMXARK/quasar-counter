8cb79b50910e4d47fb477ba147ba981d19e855f6
Роман Бушуев <bushuev@benefitty.ru>
2024-08-17 11:13:18 +0400

    feat: add q-btn remove, add
    Добавил кнопки + и - для изменения счетчика.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,27 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center text-white">
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn
+          icon="remove"
+          size="xl"
+          round
+        />
+      </div>
+      <div class="col text-center text-h2">
+        23
+      </div>
+      <div class="col text-center">
+        <q-btn
+          icon="add"
+          size="xl"
+          round
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+
 </script>
